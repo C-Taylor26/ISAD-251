5ac7d74e473597955909d2fa753a2f2d3fe21b72
Callumt683 <44776928+Callumt683@users.noreply.github.com>
2019-11-19 23:28:36 +0000

    Nav Bar
    Doesnt Work currently

--- public/index.php
+++ public/index.php
@@ -3,8 +3,17 @@
 <head>
     <meta charset="UTF-8">
     <title>Welcome!</title>
+
 </head>
 <body>
-
+    <div id="Header">
+        <link rel="navBar" href="../assets/css/navBar.css">
+        <ul>
+            <li><a class="active" href="#home">Home</a></li>
+            <li><a href="#news">News</a></li>
+            <li><a href="#contact">Contact</a></li>
+            <li><a href="#about">About</a></li>
+        </ul>
+    </div>
 </body>
 </html>
