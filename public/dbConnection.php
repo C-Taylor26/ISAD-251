56c74cc700f22233a358df0c1a7cc12ca4e30c18
Callumt683 <44776928+Callumt683@users.noreply.github.com>
2019-11-25 09:38:15 +0000

    Username and Password for db

--- public/dbConnection.php
+++ public/dbConnection.php
@@ -2,8 +2,8 @@
     function getConnection(){
         $domain = 'proj-mysql.uopnet.plymouth.ac.uk';
         $db = 'ISAD_CTaylor';
-        $user = 'CTaylor';
-        $password = 'ISAD_22213529';
+        $user = 'ISAD251_CTaylor';
+        $password = 'ISAD251_22213529';
         $mySQLConnection = new PDO("mysql:host=$domain;dbname=$db, $user, $password");
         return $mySQLConnection;
     }
